2ddaabdcbcaf6abe34bd149eb559d9e2f72f2d49
mjt89 <mjt84920002@163.com>
2017-09-16 17:02:41 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/ntian/ttydon/config/StepContant.java
@@ -0,0 +1,11 @@
+package com.ntian.ttydon.config;
+
+/**
+ * Created by mjt89 on 2017/9/16 0016.
+ */
+
+public class StepContant {
+    public static final int MSG_FROM_CLIENT = 0;
+    public static final int MSG_FROM_SERVER = 1;
+    public static final int REQUEST_SERVER = 2;
+}
